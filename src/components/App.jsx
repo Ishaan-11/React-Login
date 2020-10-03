f46d9013f3a643afef6e4685b59af16722136858
Ishaan-11 <ishaansharma1998@gmail.com>
2020-10-03 18:34:03 +0530

    Conditional Rendering for login page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
+var isLoggedIn = false;
+
+function renderConditionally() {
+  if(isLoggedIn){
+    return (<h1>Hello</h1>);
+  } else {
+    return (
       <form className="form">
         <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
         <button type="submit">Login</button>
       </form>
+    );
+  }
+}
+function App() {
+  return (
+    <div className="container">
+      {renderConditionally()}
     </div>
   );
 }
