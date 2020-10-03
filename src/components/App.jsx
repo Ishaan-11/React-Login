a293d094103b43ceda260a85950359fdcb1896cd
Ishaan-11 <ishaansharma1998@gmail.com>
2020-10-03 18:37:44 +0530

    Create Login Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Login from "./Login";
 
 var isLoggedIn = false;
 
@@ -7,11 +8,7 @@ function renderConditionally() {
     return (<h1>Hello</h1>);
   } else {
     return (
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      <Login />
     );
   }
 }
